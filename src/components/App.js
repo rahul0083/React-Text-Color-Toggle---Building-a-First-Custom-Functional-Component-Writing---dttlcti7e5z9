a9b2d5f82e9d7b2184046daf7a76485730524549
Rahul Singh <rahulme0083@gmail.com>
2023-02-21 22:29:34 +0530

    update final

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,20 @@ const App = () => {
 //code here 
 const [style,setStyle]=useState("redColor");
 const changeStyle=()=>{
+  if(style=="redColor"){
   setStyle("blueColor");
+  }
+  else{
+    setStyle("redColor");
+  }
 }
+
+
 
   return (
     <div id="main">
       <p className={style}>Newton School</p>
-      <button id='button' onClick={changeStyle}>Change Style</button>
+      <button id='button' onClick={changeStyle} >Change Style</button>
     </div>
   )
 }
